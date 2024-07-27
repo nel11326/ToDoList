9c06985fd90922232541c9e73bf7da3bc5eec585
nel11326 <lenor11326@hotmail.com>
2024-07-27 06:58:04 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my ToDoList</h1>
+      <h3>application where users can add, edit, delete, and mark tasks as completed. This project will help you understand CRUD operations and DOM manipulation.</h3>
+
+      <li>Add, edit, and delete tasks.</li>
+      <li>Mark tasks as complete or incomplete.</li>
+      <li>Filter tasks based on status (all, completed, incomplete).</li>
+    </div>
+  );
+}
+
+export default App;
